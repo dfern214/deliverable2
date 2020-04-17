b2af08507cd23627580e57702a1418c9d0b779fa
cbell088 <63829156+cbell088@users.noreply.github.com>
2020-04-17 00:57:15 -0400

    Working on Waiting View

---
+++ deliverable2/UserFormViewController.swift
@@ -0,0 +1,9 @@
+//
+//  UserFormViewController.swift
+//  deliverable2
+//
+//  Created by Christopher Bello on 4/17/20.
+//  Copyright © 2020 Dylan Fernandez. All rights reserved.
+//
+
+import Foundation
---
+++ deliverable2/WaitingViewController.swift
@@ -0,0 +1,22 @@
+//
+//  WaitingViewController.swift
+//  deliverable2
+//
+//  Created by Christopher Bello on 4/17/20.
+//  Copyright © 2020 Dylan Fernandez. All rights reserved.
+//
+
+import UIKit
+
+class WaitingListViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+    
+    
+}
+
