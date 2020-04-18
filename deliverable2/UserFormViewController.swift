63e12ef8176da036b292efa1ad84935619f7f947
Dylan <63828527+dfern214@users.noreply.github.com>
2020-04-18 11:24:29 -0700

    Fixed User submit form and added UI Picker View for gender

--- deliverable2/UserFormViewController.swift
+++ deliverable2/UserFormViewController.swift
@@ -9,16 +9,45 @@
 import UIKit
 import CoreData
 
-class UserFormViewController: UIViewController {
+class UserFormViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
-  
+    var data: [String] = [String]()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
+    @IBOutlet var firstBox: UITextField!
+    @IBOutlet var lastBox: UITextField!
+    @IBOutlet var gender: UIPickerView!
+    @IBOutlet var dateOfBirth: UIDatePicker!
+    @IBOutlet var submitButton: UIButton!
+    
+    @IBAction func submitPress(_sender: UIButton) {
         
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.gender.delegate = self
+        self.gender.dataSource = self
+        data = ["Male","Female"]
+    }
     
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // UIPICKERVIEW
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return data.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return data[row]
+    }
+    
+    // UIPICKERVIEW END
 }
