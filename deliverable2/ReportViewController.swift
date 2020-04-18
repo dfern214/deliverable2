8ca12d9721a6ede16299e6748171fc16844ad1a5
cbell088 <63829156+cbell088@users.noreply.github.com>
2020-04-18 16:10:19 -0400

    Report View Created

--- deliverable2/ReportViewController.swift
+++ deliverable2/ReportViewController.swift
@@ -6,4 +6,28 @@
 //  Copyright © 2020 Dylan Fernandez. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import CoreData
+
+class ReportViewController: UIViewController {
+    
+    @IBOutlet weak var lblDaily: UILabel!
+    @IBOutlet weak var lblMale: UILabel!
+    @IBOutlet weak var lblFemale: UILabel!
+    @IBOutlet weak var lblWeekly: UILabel!
+    @IBOutlet weak var lblAvgAge: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+    
+    
+    
+}
