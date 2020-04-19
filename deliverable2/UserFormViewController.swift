2ea0a63a3758631354899240fcb987aedbdc8e37
cbell088 <63829156+cbell088@users.noreply.github.com>
2020-04-18 20:00:50 -0400

    Committing working User Form and Report Form

--- deliverable2/UserFormViewController.swift
+++ deliverable2/UserFormViewController.swift
@@ -20,10 +20,62 @@ class UserFormViewController: UIViewController, UIPickerViewDelegate, UIPickerVi
     @IBOutlet weak var dateOfBirth: UIDatePicker!
     @IBOutlet weak var submitButton: UIButton!
     @IBOutlet weak var test: UILabel!
-    
-    @IBAction func submitPress(sender: UIButton) {
-        
+    @IBAction func submitButtonClick(_ sender: Any) {
+        do {
+            var valid = true
+            var first = ""
+            var last = ""
+            
+            let regex = try NSRegularExpression(pattern: ".*[^A-Za-z ].*", options: [])
+            if regex.firstMatch(in: firstBox.text!, options: [], range: NSMakeRange(0, (firstBox.text?.count)!)) != nil {
+                let alert = UIAlertController(title: "Warning", message: "Must not contain Number in Name", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
+                    NSLog("The \"OK\" alert occured.")}))
+                self.present(alert, animated: true, completion: nil)
+                valid = false
+            } else if firstBox.text == ""
+            {
+                let alert = UIAlertController(title: "Warning", message: "Must not be empty in Name", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
+                    NSLog("The \"OK\" alert occured.")}))
+                self.present(alert, animated: true, completion: nil)
+                valid = false
+            }
+            else{
+                first = firstBox.text!
+            }
+            
+            
+            if regex.firstMatch(in: lastBox.text!, options: [], range: NSMakeRange(0, (lastBox.text?.count)!)) != nil {
+                let alert = UIAlertController(title: "Warning", message: "Must not contain Number in Name", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
+                    NSLog("The \"OK\" alert occured.")}))
+                self.present(alert, animated: true, completion: nil)
+                valid = false
+            } else if lastBox.text == ""
+            {
+                let alert = UIAlertController(title: "Warning", message: "Must not be empty in Name", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
+                    NSLog("The \"OK\" alert occured.")}))
+                self.present(alert, animated: true, completion: nil)
+                valid = false
+            }
+            else{
+                last = lastBox.text!
+            }
+            
+            let selectedGender = data[gender.selectedRow(inComponent: 0)]
+            
+            if valid{
+                coreData.insertNewObject(first: first, last: last, birthday: dateOfBirth.date, gender: selectedGender)
+            }
+        }
+        catch {
+            
+        }
     }
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -61,4 +113,6 @@ class UserFormViewController: UIViewController, UIPickerViewDelegate, UIPickerVi
     }
     
     // UIPICKERVIEW END
+    
+    
 }
